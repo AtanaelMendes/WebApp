deccccb5affde5bc7f8f39a1a212ca6deca853b8
AtanaelMendes <atanael_snp@hotmail.com>
2018-09-18 10:50:19 -0400

    [ADD] criando rotas de login e index

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,29 +1,6 @@
+export default [
 
-const routes = [
+  { path: '/index', component: () => import('components/Index.vue') },
+  { path: '/login',  component: () => import('components/Login.vue') }
 
- //  // Marca
- // { path: '/marca', component: () => import('components/views/marca/Index') },
- // { path: '/marca/create', component: () => import('components/views/marca/Create') },
- // { path: '/marca/:id', component: () => import('components/views/marca/View') },
- // { path: '/marca/:id/foto', component: () => import('components/views/marca/Photo') },
- // { path: '/marca/:id/update', component: () => import('components/views/marca/Update') },
-
-  {
-    path: '/',
-    // component: () => import('layouts/AgroLayout.vue'),
-    component: () => import('components/Index.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
-  }
 ]
-
-// Always leave this as last one
-if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('pages/Error404.vue')
-  })
-}
-
-export default routes
