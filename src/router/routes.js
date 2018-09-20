4be1e9aabd47041e3d770152febab9c66271bbe2
AtanaelMendes <atanael_snp@hotmail.com>
2018-09-20 17:57:36 -0400

    [ADD] adicionado link rota 404

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,6 +1,7 @@
 export default [
 
   { path: '/', component: () => import('components/Index.vue') },
-  { path: '/Login',  component: () => import('components/Login.vue') }
+  { path: '/Login',  component: () => import('components/Login.vue') },
+  { path: '*', component: () => import('components/Error404') }
 
 ]
