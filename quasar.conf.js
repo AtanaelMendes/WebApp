8d158aa6ccd774510e003e789f74307d54594286
AtanaelMendes <atanael_snp@hotmail.com>
2018-09-20 10:00:54 -0400

    [ADD] instalando vue validate

--- quasar.conf.js
+++ quasar.conf.js
@@ -5,7 +5,8 @@ module.exports = function (ctx) {
   return {
     // app plugins (/src/plugins)
     plugins: [
-      'axios'
+      'axios',
+      'vuelidate'
     ],
     css: [
       'app.styl'
