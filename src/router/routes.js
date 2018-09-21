816d906b112d0be6da0089b3374c1184186bd94e
AtanaelMendes <atanael_snp@hotmail.com>
2018-09-21 17:25:39 -0400

    [ADD] rota de exemplo

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,8 @@ export default [
 
   { path: '/', component: () => import('components/Index.vue') },
   { path: '/Login',  component: () => import('components/Login.vue') },
-  { path: '*', component: () => import('components/Error404') }
+  { path: '*', component: () => import('components/Error404.vue') },
 
+  // rota de exemplo
+  { path: '/exemplo',  component: () => import('components/views/example/Index.vue') }
 ]
