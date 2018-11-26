17e9ea765524a3675491886afa08a2f8b911d2b5
AtanaelMendes <atanael_snp@hotmail.com>
2018-11-26 15:18:58 -0300

    [upd] melhoria no select-localizacao

--- src/assets/js/service/localizacao/LocalizacaoService.js
+++ src/assets/js/service/localizacao/LocalizacaoService.js
@@ -10,6 +10,22 @@ export default {
       })
     });
   },
+  listLocalizacao: function(){
+    return new Promise((resolve, reject) => {
+      this.listLocalizacoesByProdutor(localStorage.getItem('account.produtor_id')).then(response => {
+        let localizacaoOptions = response.data.map(local => {
+          return {
+            value: local.id,
+            label: local.endereco +', '+ local.numero,
+            sublabel: local.bairro +', '+ local.cidade.nome +'-'+ local.cidade.estado.sigla
+          }
+        });
+        resolve(localizacaoOptions)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
   listLocalizacoes(id){
     return new Promise((resolve, reject) => {
       Vue.prototype.$axios.get( 'pessoa/' + id + '/localizacao' ).then( response => {
