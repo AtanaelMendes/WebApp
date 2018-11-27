aafd88e587718c531349bed89a6044ba63a08a56
AtanaelMendes <atanael_snp@hotmail.com>
2018-11-27 17:53:48 -0300

    [upd] adicionado o produtor e implementado model

---
+++ src/assets/js/model/usuario/Usuario.js
@@ -0,0 +1,80 @@
+import { helpers} from 'vuelidate/lib/validators'
+export default class{
+  email = {
+    value: null,
+    errorMessage: null
+  };
+  produtor = {
+    value: null,
+    errorMessage: null
+  };
+  password = {
+    value: null,
+    errorMessage: null
+  };
+  repeatPassword = {
+    value: null,
+    errorMessage: null
+  };
+  selectedRoles = {
+    value: [],
+    errorMessage: null
+  };
+  constructor(usuario){
+    if(usuario !== undefined){
+      this.email.value = usuario.email.value;
+      this.produtor.value = usuario.produtor.value;
+      this.password.value = usuario.password.value;
+      this.repeatPassword.value = usuario.repeatPassword.value;
+      this.selectedRoles.value = usuario.selectedRoles.value;
+    }
+  };
+  isValid(){
+    let hasError = false;
+    if(!helpers.req(this.email.value)){
+      this.email.errorMessage = "Digite um email";
+      hasError = true;
+    }
+
+    if(!helpers.req(this.produtor.value)){
+      this.produtor.errorMessage = "Selecione um Produtor";
+      hasError = true;
+    }
+
+    if (!helpers.req(this.password.value)) {
+      this.password.errorMessage = "Confirme a senha";
+      hasError = true;
+    }
+
+    if(helpers.len(this.password.value) < 8){
+      this.password.errorMessage = "A senha deve ter no mínimo 8 caracteres";
+      hasError = true;
+    }
+
+    if (!helpers.req(this.repeatPassword.value)) {
+      this.repeatPassword.errorMessage = "Confirme a senha";
+      hasError = true;
+    }
+
+    if (this.repeatPassword.value != this.password.value) {
+      this.repeatPassword.errorMessage = "As senhas não conferem";
+      hasError = true;
+    }
+
+    if (!helpers.req(this.selectedRoles.value)) {
+      this.selectedRoles.errorMessage = "Escolha ao menos um Papel para o usuário";
+      hasError = true;
+    }
+    return !hasError;
+  };
+
+  getValues(){
+    return{
+      email: this.email.value,
+      produtor_id: this.produtor.value,
+      password: this.password.value,
+      selectedRoles: this.selectedRoles.value,
+    }
+  };
+
+}
--- src/pages/admin/users/UserAdd.vue
+++ src/pages/admin/users/UserAdd.vue
@@ -9,30 +9,39 @@
         <form>
           <q-list-header class="q-pa-none">Informações Básicas</q-list-header>
 
-          <produtor-select @click.native="listProdutor" label="Produtor" :model="form.produtor" :options="produtorOptions"/>
+          <q-item class="q-px-none">
+            <q-item-main>
+              <produtor-select @click.native="listProdutor" label="Produtor" :model="usuario.produtor" :options="produtorOptions"/>
+            </q-item-main>
+            <q-item-side>
+              <q-btn color="deep-orange" round icon="add" @click.native="openNewProdutorDialog"/>
+            </q-item-side>
+          </q-item>
 
-          <custom-input-text type="email" label="Email" :model="form.email" />
+          <custom-input-text type="email" label="Email" :model="usuario.email" />
 
-          <custom-input-text type="password" label="Senha" :model="form.password" />
+          <custom-input-text type="password" label="Senha" :model="usuario.password" />
 
-          <custom-input-text type="password" label="Confirmar senha" :model="form.repeatPassword" />
+          <custom-input-text type="password" label="Confirmar senha" :model="usuario.repeatPassword" />
         </form>
       </div>
 
       <!--LIST ROLES-->
       <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
         <q-list-header class="q-pa-none">Funções</q-list-header>
-        <q-field :error="form.selectedRoles.errorMessage != null">
-          <q-list id="chip_container"
-                  v-if="form.selectedRoles.value"
-                  class="chip-container"
-                  :class="{ 'chip-container-error': form.selectedRoles.errorMessage != null }">
-            <q-item v-for="role in form.selectedRoles.value" :key="role.id" class="chip-inline">
+        <q-field :error="usuario.selectedRoles.errorMessage != null">
+          <q-list
+            id="chip_container"
+            class="chip-container"
+            v-if="usuario.selectedRoles.value"
+            :class="{ 'chip-container-error': usuario.selectedRoles.errorMessage != null }"
+          >
+            <q-item v-for="role in usuario.selectedRoles.value" :key="role.id" class="chip-inline">
               <q-chip class="q-ma-xs" @hide="removeRole(role)" closable color="secondary" text-color="white">{{role.name}}</q-chip>
             </q-item>
           </q-list>
           <div class="q-field-bottom row no-wrap q-field-no-input-fix" style="height: 22px">
-            <div class="q-field-error col" v-if="form.selectedRoles.errorMessage != null" >{{form.selectedRoles.errorMessage}}</div>
+            <div class="q-field-error col" v-if="usuario.selectedRoles.errorMessage != null" >{{usuario.selectedRoles.errorMessage}}</div>
           </div>
         </q-field>
 
@@ -48,7 +57,7 @@
 
       <div slot="body">
         <form @keyup.enter="saveProdutor()">
-          <custom-input-text type="text" label="Produtor" :model="produtor.nome" />
+          <custom-input-text type="text" label="Nome" :model="produtor.nome" />
         </form>
       </div>
 
@@ -65,11 +74,10 @@
   import toolbar from 'components/Toolbar.vue'
   import customPage from 'components/CustomPage.vue'
   import customInputText from 'components/CustomInputText.vue'
-  import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
   import userService from 'assets/js/service/UserService'
   import produtor from 'assets/js/model/produtor/Produtor'
+  import user from 'assets/js/model/usuario/Usuario'
   import produtorSelect from 'components/ProdutorSelect.vue'
-  import FormMixin from 'components/mixins/FormMixin'
   export default {
     name: "user-add",
     components: {
@@ -78,7 +86,6 @@
       produtorSelect,
       customInputText
     },
-    mixins: [FormMixin],
     data(){
       return {
         roles: null,
@@ -87,96 +94,33 @@
         newProdutorDialog: false,
         produtor: new produtor(),
         produtorOptions: [],
-        form: {
-          email: {
-            value: null,
-            errorMessage: null
-          },
-          produtor: {
-            value: null,
-            errorMessage: null
-          },
-          password: {
-            value: null,
-            errorMessage: null
-          },
-          repeatPassword: {
-            value: null,
-            errorMessage: null
-          },
-          selectedRoles: {
-            value: [],
-            errorMessage: null
-          },
-        }
-      }
-    },
-    validations: {
-      form: {
-        email: { value: { required, email } },
-        password: { value: {required, minLength: minLength(8) } },
-        repeatPassword: { value: {function () {
-              return this.form.repeatPassword.value === this.form.password.value;
-            }}},
-        selectedRoles: { value: { required } }
+        usuario: new user()
       }
     },
     methods:{
       openRolesDialog: function(){
-        userService.openRolesDialog(this.form.selectedRoles.value, this.roles).then(roles =>{
-          this.form.selectedRoles.errorMessage = null;
-          this.form.selectedRoles.value = roles;
+        userService.openRolesDialog(this.usuario.selectedRoles.value, this.roles).then(roles =>{
+          this.usuario.selectedRoles.errorMessage = null;
+          this.usuario.selectedRoles.value = roles;
         })
       },
       removeRole: function(role){
-        userService.removeRole(this.form.selectedRoles.value, role);
+        userService.removeRole(this.usuario.selectedRoles.value, role);
       },
       listRoles: function(){
         userService.listRoles().then(roles => {this.roles = roles})
       },
       saveAccount: function () {
-        this.$v.form.$touch();
-        if ( this.$v.form.$error ) {
-          if(!this.$v.form.email.value.required){
-            this.form.email.errorMessage = "Digite um email"
-          }else if(!this.$v.form.email.value.email){
-            this.form.email.errorMessage = "Este email é inválido"
-          }
-
-          if(!this.$v.form.password.value.required){
-            this.form.password.errorMessage = "Digite uma senha"
-          }else if(!this.$v.form.password.value.minLength){
-            this.form.password.errorMessage = "A senha deve ter no mínimo 8 caracteres"
-          }
-          if(!this.$v.form.repeatPassword.value.required){
-            this.form.repeatPassword.errorMessage = "As senhas não são iguais"
-          }
-          if(!this.$v.form.selectedRoles.value.required){
-            this.form.selectedRoles.errorMessage = "Adicione ao menos uma função"
-          }
+        if(!this.usuario.isValid()){
           return;
         }
-        let params = {
-          email: this.form.email.value,
-          password: this.form.password.value,
-          roles: userService.getIdsByRoles(this.form.selectedRoles.value).join()
-
-        };
-        userService.saveAccount(params).then(response => {
-          this.$q.notify({
-            type: 'positive',
-            message: 'Cadastro criado com sucesso'
-          });
-
-          this.setFormObj(this.form);
+        userService.saveAccount(this.usuario.getValues()).then(response => {
+          this.$q.notify({type: 'positive', message: 'Cadastro criado com sucesso'});
           this.$router.push({name: 'users'});
           this.$root.$emit('refreshUserList')
         }).catch(error => {
           if (error.response.status === 422){
-            this.$q.dialog({
-              title:'Ops',
-              message: 'Já existe um cadastro com esse email'
-            })
+            this.$q.dialog({title:'Ops', message: 'Já existe um cadastro com esse email'})
           }
         })
       },
@@ -211,8 +155,6 @@
       }
     },
     mounted(){
-      this.routeName = 'add_user';
-      this.setFormObj(this.form);
       this.listRoles();
     },
 
