9b318712ce2c24905b90117b42c5c3a1ae1a3600
Danilo O. Lima <danilo__oliveira@hotmail.com>
2018-11-28 16:59:48 -0300

    Adicionado compo nome na conta

--- src/assets/js/model/usuario/Usuario.js
+++ src/assets/js/model/usuario/Usuario.js
@@ -1,5 +1,9 @@
 import {email, helpers} from 'vuelidate/lib/validators'
 export default class{
+  nome = {
+    value: null,
+    errorMessage: null
+  };
   email = {
     value: null,
     errorMessage: null
@@ -22,6 +26,7 @@ export default class{
   };
   constructor(usuario){
     if(usuario !== undefined){
+      this.nome.value = usuario.nome.value;
       this.email.value = usuario.email.value;
       this.produtor.value = usuario.produtor.value;
       this.password.value = usuario.password.value;
@@ -31,6 +36,15 @@ export default class{
   };
   isValid(){
     let hasError = false;
+
+    if(!helpers.req(this.nome.value)){
+      this.nome.errorMessage = "Digite um nome";
+      hasError = true;
+    }else if(helpers.len(this.nome.value) < 3){
+      this.nome.errorMessage = "O nome deve ter no mínimo 3 caracteres";
+      hasError = true;
+    }
+
     if(!helpers.req(this.email.value)){
       this.email.errorMessage = "Digite um email";
       hasError = true;
@@ -73,6 +87,7 @@ export default class{
 
   getValues(){
     return{
+      nome: this.nome.value,
       email: this.email.value,
       produtor_id: this.produtor.value,
       password: this.password.value,
