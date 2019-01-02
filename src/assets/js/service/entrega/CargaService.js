26e2c7a372ec678f215f1e97d1b6c9ee0aad2666
AtanaelMendes <atanael_snp@hotmail.com>
2019-01-02 19:35:07 -0300

    [upd] finalizado tela de envio de carga

---
+++ src/assets/js/service/entrega/CargaService.js
@@ -0,0 +1,87 @@
+import Vue from 'vue'
+import { Loading, Dialog } from 'quasar'
+const produtorId = localStorage.getItem('account.produtor_id');
+export default {
+  listCargasCarregando(){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.get( 'produtor/'+ produtorId + '/entregas?staus=carregando').then( response => {
+        resolve(response);
+      }).catch(error => {
+        reject(error)
+      })
+    });
+  },
+  listCargasNoArmazem(){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.get( 'produtor/'+ produtorId + '/entregas?staus=no-armazem').then( response => {
+        resolve(response);
+      }).catch(error => {
+        reject(error)
+      })
+    });
+  },
+  listCargasEntregues(){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.get( 'produtor/'+ produtorId + '/entregas?staus=entregues').then( response => {
+        resolve(response);
+      }).catch(error => {
+        reject(error)
+      })
+    });
+  },
+
+  getCargaById(id){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.get( 'produtor/'+ produtorId + '/entrega/'+ id).then( response => {
+        resolve(response);
+      }).catch(error => {
+        reject(error)
+      })
+    });
+  },
+  saveNewCarga(params){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.post('produtor/'+ produtorId + '/entrega', params).then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error.response)
+      })
+    });
+  },
+  updateEntrega(id, params){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.put('produtor/'+ produtorId + '/entrega/'+ id, params).then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error.response)
+      })
+    });
+  },
+  archiveEntrega(id){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.put('produtor/'+ produtorId + '/entrega/' + id + '/archive').then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error.response)
+      })
+    });
+  },
+  restoreEntrega(id){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.put('produtor/'+ produtorId + '/entrega/' + id +'/restore').then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error.response)
+      })
+    });
+  },
+  deleteEntrega(id){
+    return new Promise((resolve, reject) => {
+      Vue.prototype.$axios.delete('produtor/'+ produtorId + '/entrega/' + id).then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error.response)
+      })
+    });
+  },
+}
