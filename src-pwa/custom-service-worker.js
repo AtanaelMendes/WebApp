a48b6a7eeda3ca1bb60381b73d1a70f15174e285
Danilo O. Lima <danilo__oliveira@hotmail.com>
2019-02-05 09:19:34 -0300

    Melhorado o fetch do service worker

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -4,16 +4,44 @@
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
 
-/*
 self.addEventListener('fetch', function(event) {
   if(event.request.method === "GET"){
     event.respondWith(
+      caches.open('agro_project').then(function(cache) {
+        return fetch(event.request).then(function (response) {
+          console.log('nova requisição')
+          cache.put(event.request, response.clone());
+          return response;
+        }).catch(function () {
+          console.log('pegando do cache')
+          return caches.match(event.request).catch(function () {
+            console.log('erro no cache')
+          })
+        })
+      })
+    );
+  }
+});
+
+/*self.addEventListener('fetch', function(event) {
+  if(event.request.method === "GET"){
+    console.log("event.request")
+    //console.log(event.request)
+    event.respondWith(
       caches.match(event.request).then(function(response) {
         if(response){
+          console.log('cache.response')
+          console.log(response)
+          console.log('etag:' + response.headers.get('etag'))
           return response
         }else{
           saveOnCache(event.request);
-          return fetch(event.request)
+          return fetch(event.request).then(function (response) {
+            console.log('fetch.response')
+            console.log(response)
+            console.log('etag:' + response.headers.get('etag'))
+            return response;
+          })
         }
       })
     );
@@ -27,7 +55,6 @@ function saveOnCache(request) {
       return response;
     });
   });
-}
-*/
+}*/
 
 
