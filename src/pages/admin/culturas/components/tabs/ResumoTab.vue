65b31885f8d3caafd3958a1099cff234d6bc3625
Danilo O. Lima <danilo__oliveira@hotmail.com>
2019-04-23 15:06:10 -0400

    [wip]Implementando tab de resumo em culturas

--- src/pages/admin/culturas/components/tabs/ResumoTab.vue
+++ src/pages/admin/culturas/components/tabs/ResumoTab.vue
@@ -1,20 +1,50 @@
 <template>
-    
+  <div class="row space-end" v-if="visible">
+    <div class="col-xs-12 col-sm-5 col-md-5 col-lg-5 col-xl-5">
+      <q-card-media overlay-position="top" style="max-height: 40vh">
+        <ap-image size="800x500" :file-name="cultura.image_file_name"/>
+        <q-card-title slot="overlay">
+          {{cultura.nome}}
+          <div slot="right" class="row items-center">
+            <q-btn flat round dense color="white" icon="more_vert" >
+              <q-popover anchor="bottom left">
+                <q-list link>
+                  <q-item v-close-overlay @click.native="openChangePhotoModal(cultura.id)">
+                    <q-item-main label="Alterar Imagem"/>
+                  </q-item>
+                </q-list>
+              </q-popover>
+            </q-btn>
+          </div>
+        </q-card-title>
+      </q-card-media>
+    </div>
+    <div class="col-xs-12 col-sm-7 col-md-7 col-lg-7 col-xl-7">a</div>
+  </div>
 </template>
 
 <script>
+  import apImage from 'components/ApImage'
   export default {
     name: "ResumoTab",
+    components:{
+      apImage
+    },
     props:{
       visible: false
     },
     data() {
-      return {}
+      return {
+        cultura: null,
+      }
     },
     methods: {
-      init(safraCultura) {
-        this.safraCultura = safraCultura;
+      init(cultura) {
+        this.cultura = cultura;
       },
+      openChangePhotoModal(culturaId){
+
+      }
     }
   }
 </script>
