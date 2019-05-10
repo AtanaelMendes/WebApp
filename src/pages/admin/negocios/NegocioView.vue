e07d88138ac56070fdf664cb5d2196010eb27515
Danilo O. Lima <danilo__oliveira@hotmail.com>
2019-05-10 14:36:33 -0400

    Exibindo as informações de resumo da parte fiscal

--- src/pages/admin/negocios/NegocioView.vue
+++ src/pages/admin/negocios/NegocioView.vue
@@ -132,7 +132,7 @@
                                 {{armazem.nome}}
                               </q-item-main>
                               <q-item-side right>
-                                {{numeral(armazem.quantidade).format('0,0')}} {{cultura.unidade_medida.sigla}}
+                                {{numeral(armazem.quantidade_entrega).format('0,0')}} {{cultura.unidade_medida.sigla}}
                               </q-item-side>
                             </q-item>
                             <q-item class="q-body-1">
@@ -158,7 +158,26 @@
                         <q-tab slot="title" :name="'tab-' + index" :label="armazemTitulo" v-for="(armazemTitulo, index) in armazensTitulos(cultura.armazens)" :key="armazemTitulo"/>
 
                         <q-tab-pane class="q-pa-none" name="tab-resumo">
-
+                          <!--<q-list no-border>
+                            <q-item class="q-body-1">
+                              <q-item-main>
+                                Total
+                              </q-item-main>
+                              <q-item-side right>
+                                R$ {{ numeral(somaNotasFiscais(cultura.armazens)).format('0,0.00') }}
+                              </q-item-side>
+                            </q-item>
+                            <q-item v-for="armazem in cultura.armazens" :key="armazem.id" class="q-body-1">
+                              <q-item-main>
+                                {{armazem.nome}}
+                              </q-item-main>
+                              <q-item-side right>
+                                R$ {{numeral(armazem.valor_total_nf).format('0,0.00')}}
+                              </q-item-side>
+                            </q-item>
+                          </q-list>-->
+                          <q-table :data="getResumoFiscalData(cultura.armazens)" :columns="fiscalResumoColumns" row-key="id">
+                          </q-table>
                         </q-tab-pane>
                         <q-tab-pane class="q-pa-none" :name="'tab-' + index" v-for="(armazem, index) in cultura.armazens" :key="armazem.id" keep-alive>
                           <armazem-notas-list-tabs :negocio-cultura="cultura" :armazem="armazem" />
@@ -500,6 +519,13 @@
         negocioService: new NegocioService(),
         negocio: null,
         progressModel: 50,
+        fiscalResumoColumns: [
+          {label: 'Armazém', name:'armazem', field:'armazem', align: 'left',},
+          {label: 'Peso Fiscal', name:'peso_fiscal', field:'peso_fiscal', align: 'left',},
+          {label: 'Peso Físico', name:'peso_fisico', field:'peso_fisico', align: 'left',},
+          {label: 'Preço Fiscal', name:'preco_fiscal', field:'preco_fiscal', align: 'left',},
+          {label: 'Preço Físico', name:'preco_fisico', field:'preco_fisico', align: 'left',},
+        ],
       }
     },
     methods: {
@@ -642,6 +668,32 @@
       },
       armazensTitulos(armazens){
         return _.map(armazens, 'nome');
+      },
+      somaNotasFiscais(armazens){
+        let total = 0;
+
+        for(let armazem of armazens){
+          total += armazem.valor_total_nf;
+        }
+
+        return total;
+      },
+      getResumoFiscalData(armazens){
+
+        let result = [];
+
+        for(let armazem of armazens){
+          result.push({
+            'id': armazem.id,
+            'armazem': armazem.nome,
+            'peso_fiscal': this.numeral(armazem.pesagem_total_nf).format('0,0'),
+            'peso_fisico': this.numeral(armazem.pesagem_total_entregas).format('0,0'),
+            'preco_fiscal': 'R$ ' + this.numeral(armazem.valor_total_nf).format('0,0.00'),
+            'preco_fisico': 'R$ ' + this.numeral(armazem.valor_total_entregas).format('0,0.00'),
+          });
+        }
+
+        return result;
       },
       backAction () {
         this.$router.back();
