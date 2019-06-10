2229e0b3190f09c218aa6c3f60df497a0b16aa4a
Danilo O. Lima <danilo__oliveira@hotmail.com>
2019-06-10 08:45:05 -0400

    Adicionado link para redirecionar para entregas

--- src/pages/admin/negocios/components/tabs/ArmazemEntregasListTab.vue
+++ src/pages/admin/negocios/components/tabs/ArmazemEntregasListTab.vue
@@ -8,7 +8,7 @@
         </div>
 
         <template v-if="movimentos">
-          <q-list separator dense v-if="movimentos.length > 0">
+          <q-list separator  dense v-if="movimentos.length > 0">
 
             <q-item style="padding-right: unset" class="gt-xs">
               <q-item-main class="row">
@@ -58,6 +58,7 @@
             </q-item>
 
             <template v-for="movimento in movimentos">
+              <a v-bind:href="entregaUrl(movimento.entrega)" class="link">
               <q-item style="padding-right: unset" :key="movimento.id" v-if="$q.screen.gt.xs">
                 <q-item-main class="row ">
 
@@ -172,128 +173,130 @@
               </q-item>
 
               <q-item class="lt-sm" style="padding: 3px 8px 3px 8px" :key="movimento.id" v-else>
-                <q-item-main>
+
+                <q-item-main >
                   <q-item-tile class="row">
-                    <div class="col-2">
-                      <div class="col-1 text-center table-cell">
-                        <abbr :title="movimento.negocio_cultura_movimento_tipo.nome">
-                          <q-chip dense square color="blue-8" v-if="movimento.negocio_cultura_movimento_tipo.sigla==='ENT'">{{movimento.negocio_cultura_movimento_tipo.sigla}}</q-chip>
-                          <q-chip dense square color="amber" v-else-if="movimento.negocio_cultura_movimento_tipo.sigla==='TRA'">{{movimento.negocio_cultura_movimento_tipo.sigla}}</q-chip>
-                          <q-chip dense square color="deep-purple" v-else >{{movimento.negocio_cultura_movimento_tipo.sigla}}</q-chip>
-                        </abbr>
+                      <div class="col-2">
+                        <div class="col-1 text-center table-cell">
+                          <abbr :title="movimento.negocio_cultura_movimento_tipo.nome">
+                            <q-chip dense square color="blue-8" v-if="movimento.negocio_cultura_movimento_tipo.sigla==='ENT'">{{movimento.negocio_cultura_movimento_tipo.sigla}}</q-chip>
+                            <q-chip dense square color="amber" v-else-if="movimento.negocio_cultura_movimento_tipo.sigla==='TRA'">{{movimento.negocio_cultura_movimento_tipo.sigla}}</q-chip>
+                            <q-chip dense square color="deep-purple" v-else >{{movimento.negocio_cultura_movimento_tipo.sigla}}</q-chip>
+                          </abbr>
+                        </div>
                       </div>
-                    </div>
-                    <div class="col-5 text-left table-cell">
-                      <div :class="(movimento.quantidade<0)?'text-negative':'text-indigo'">
-                        {{numeral(movimento.quantidade).format('0,0')}} KG
-                      </div>
-                      <div class="text-faded">
-                        {{numeral(movimento.quantidade_comercial).format('0,0')}} SC
-                      </div>
-                    </div>
-
-                    <div class="col-4 text-right table-cell text-faded">
-                      {{moment(movimento.lancamento).format('DD/MMM/YY')}}
-                      <div class="text-faded ">
-                        {{moment(movimento.lancamento).format('HH:mm:ss')}}
-                      </div>
-                    </div>
-
-                    <div class="col-1 text-right">
-                      <div v-if="movimento.negocio_cultura_transferencia">
-                        <q-btn flat dense icon="more_vert" round class="q-mr-sm" v-if="movimento.negocio_cultura_transferencia.is_editable">
-                          <q-popover>
-                            <q-list link class="no-border">
-                              <q-item v-close-overlay @click.native="editTransferencia(movimento.id)">
-                                <q-item-main label="Editar Transferência"/>
-                              </q-item>
-                              <q-item v-close-overlay @click.native="deleteTransferencia(movimento.id)">
-                                <q-item-main label="Apagar Transferência"/>
-                              </q-item>
-                            </q-list>
-                          </q-popover>
-                        </q-btn>
+                      <div class="col-5 text-left table-cell">
+                        <div :class="(movimento.quantidade<0)?'text-negative':'text-indigo'">
+                          {{numeral(movimento.quantidade).format('0,0')}} KG
+                        </div>
+                        <div class="text-faded">
+                          {{numeral(movimento.quantidade_comercial).format('0,0')}} SC
+                        </div>
                       </div>
 
-                      <div v-else-if="movimento.negocio_cultura_movimento_tipo.sigla === 'ENT'">
+                      <div class="col-4 text-right table-cell text-faded">
+                        {{moment(movimento.lancamento).format('DD/MMM/YY')}}
+                        <div class="text-faded ">
+                          {{moment(movimento.lancamento).format('HH:mm:ss')}}
+                        </div>
                       </div>
 
-                      <div v-else>
-                        <q-btn flat dense icon="more_vert" round class="q-mr-sm">
-                          <q-popover>
-                            <q-list link class="no-border">
-                              <q-item v-close-overlay @click.native="editMovimento(movimento.id)">
-                                <q-item-main label="Editar Movimento"/>
-                              </q-item>
-                              <q-item v-close-overlay @click.native="deleteMovimento(movimento.id)">
-                                <q-item-main label="Apagar Movimento"/>
-                              </q-item>
-                            </q-list>
-                          </q-popover>
-                        </q-btn>
+                      <div class="col-1 text-right">
+                        <div v-if="movimento.negocio_cultura_transferencia">
+                          <q-btn flat dense icon="more_vert" round class="q-mr-sm" v-if="movimento.negocio_cultura_transferencia.is_editable">
+                            <q-popover>
+                              <q-list link class="no-border">
+                                <q-item v-close-overlay @click.native="editTransferencia(movimento.id)">
+                                  <q-item-main label="Editar Transferência"/>
+                                </q-item>
+                                <q-item v-close-overlay @click.native="deleteTransferencia(movimento.id)">
+                                  <q-item-main label="Apagar Transferência"/>
+                                </q-item>
+                              </q-list>
+                            </q-popover>
+                          </q-btn>
+                        </div>
+
+                        <div v-else-if="movimento.negocio_cultura_movimento_tipo.sigla === 'ENT'">
+                        </div>
+
+                        <div v-else>
+                          <q-btn flat dense icon="more_vert" round class="q-mr-sm">
+                            <q-popover>
+                              <q-list link class="no-border">
+                                <q-item v-close-overlay @click.native="editMovimento(movimento.id)">
+                                  <q-item-main label="Editar Movimento"/>
+                                </q-item>
+                                <q-item v-close-overlay @click.native="deleteMovimento(movimento.id)">
+                                  <q-item-main label="Apagar Movimento"/>
+                                </q-item>
+                              </q-list>
+                            </q-popover>
+                          </q-btn>
+                        </div>
+
                       </div>
 
-                    </div>
-
-                  </q-item-tile>
+                    </q-item-tile>
 
                   <q-item-tile class="row q-pt-sm">
-                    <div class="col-12">
-                      <!-- DESCRICAO -->
-                      <div v-if="movimento.descricao">
-                        <div class="row">
-                          <div class="col-12 text-faded table-cell">
-                            {{ movimento.descricao }}
-                          </div>
-                        </div>
-                      </div>
-
-                      <!-- TRANSFERENCIAS -->
-                      <div v-if="movimento.negocio_cultura_transferencia" class="table-cell">
-                        <div class="row" v-for="tmov in movimento.negocio_cultura_transferencia.negocios_culturas_movimentos">
-                          {{tmov.negocio_cultura.negocio.pessoa}} -
-                          {{tmov.negocio_cultura.negocio.numero_contrato}} -
-                          {{tmov.negocio_cultura.negocio.numero_pedido}}
-                        </div>
-                        <div class="row">
-                          <div class="col-12 text-faded">
-                            {{ movimento.negocio_cultura_transferencia.descricao }}
-                          </div>
-                        </div>
-                      </div>
-
-                      <!-- ENTREGAS-->
-                      <div class="row" v-if="movimento.entrega">
-                        <div class="col-3 table-cell">
-                          <span class="text-weight-regular q-caption">Placa</span><br/>
-                          <span class="q-caption text-weight-light">{{movimento.entrega.caminhao.placa }}</span>
-                        </div>
-                        <div class="col-9">
-                          <div class="row " v-for="pesagem in movimento.entrega.entregas_pesagens">
-                            <div class="col-3 table-cell ">
-                              <span class="text-weight-regular q-caption">Nº Ticket</span><br/>
-                              <span class="q-caption text-weight-light">{{pesagem.numero_ticket}}</span>
-                            </div>
-                            <div class="col-3 table-cell">
-                              <span class="text-weight-regular q-caption">Bruto</span><br/>
-                              <span class="q-caption text-weight-light">{{numeral(pesagem.peso_bruto_produto).format('0,00')}} {{negocioCultura.safra_cultura.unidade_medida_pesagem.sigla}}</span>
-                            </div>
-                            <div class="col-3 table-cell">
-                              <span class="text-weight-regular q-caption">Desconto</span><br/>
-                              <span class="q-caption text-weight-light">{{numeral(pesagem.peso_desconto).format('0,00')}} {{negocioCultura.safra_cultura.unidade_medida_pesagem.sigla}}</span>
-                            </div>
-                            <div class="col-3 table-cell">
-                              <span class="text-weight-regular q-caption">Líquido</span><br/>
-                              <span class="q-caption text-weight-light">{{numeral(pesagem.peso_liquido).format('0,00')}} {{negocioCultura.safra_cultura.unidade_medida_pesagem.sigla}}</span>
+                      <div class="col-12">
+                        <!-- DESCRICAO -->
+                        <div v-if="movimento.descricao">
+                          <div class="row">
+                            <div class="col-12 text-faded table-cell">
+                              {{ movimento.descricao }}
                             </div>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  </q-item-tile>
 
+                        <!-- TRANSFERENCIAS -->
+                        <div v-if="movimento.negocio_cultura_transferencia" class="table-cell">
+                          <div class="row" v-for="tmov in movimento.negocio_cultura_transferencia.negocios_culturas_movimentos">
+                            {{tmov.negocio_cultura.negocio.pessoa}} -
+                            {{tmov.negocio_cultura.negocio.numero_contrato}} -
+                            {{tmov.negocio_cultura.negocio.numero_pedido}}
+                          </div>
+                          <div class="row">
+                            <div class="col-12 text-faded">
+                              {{ movimento.negocio_cultura_transferencia.descricao }}
+                            </div>
+                          </div>
+                        </div>
+
+                        <!-- ENTREGAS-->
+                        <div class="row" v-if="movimento.entrega">
+                          <div class="col-3 table-cell">
+                            <span class="text-weight-regular q-caption">Placa</span><br/>
+                            <span class="q-caption text-weight-light">{{movimento.entrega.caminhao.placa }}</span>
+                          </div>
+                          <div class="col-9">
+                            <div class="row " v-for="pesagem in movimento.entrega.entregas_pesagens">
+                              <div class="col-3 table-cell ">
+                                <span class="text-weight-regular q-caption">Nº Ticket</span><br/>
+                                <span class="q-caption text-weight-light">{{pesagem.numero_ticket}}</span>
+                              </div>
+                              <div class="col-3 table-cell">
+                                <span class="text-weight-regular q-caption">Bruto</span><br/>
+                                <span class="q-caption text-weight-light">{{numeral(pesagem.peso_bruto_produto).format('0,00')}} {{negocioCultura.safra_cultura.unidade_medida_pesagem.sigla}}</span>
+                              </div>
+                              <div class="col-3 table-cell">
+                                <span class="text-weight-regular q-caption">Desconto</span><br/>
+                                <span class="q-caption text-weight-light">{{numeral(pesagem.peso_desconto).format('0,00')}} {{negocioCultura.safra_cultura.unidade_medida_pesagem.sigla}}</span>
+                              </div>
+                              <div class="col-3 table-cell">
+                                <span class="text-weight-regular q-caption">Líquido</span><br/>
+                                <span class="q-caption text-weight-light">{{numeral(pesagem.peso_liquido).format('0,00')}} {{negocioCultura.safra_cultura.unidade_medida_pesagem.sigla}}</span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </q-item-tile>
                 </q-item-main>
+
               </q-item>
+              </a>
             </template>
 
           </q-list>
@@ -347,53 +350,11 @@
       editMovimentoModal,
       editTransferenciaModal
     },
-    computed:{
-      movimentosMaped(){
-        return this.movimentos.map(movimento => {
-          let movimentoMapped = {
-            'id': movimento.id,
-            'tipo': movimento.tipo.nome,
-            'data': this.moment(movimento.data).format('DD/MMM/YYYY'),
-            'p_bruto': this.numeral(movimento.peso_bruto).format('0,0') + " " + movimento.unidade_medida_sigla,
-            'p_desconto': this.numeral(movimento.peso_desconto).format('0,0') + " " + movimento.unidade_medida_sigla,
-            'p_liquido': this.numeral(movimento.peso_liquido).format('0,0') + " " + movimento.unidade_medida_sigla,
-          };
-
-          if(movimento.tipo.nome === 'Entrega'){
-            movimentoMapped.entrega_id = movimento.entrega.id;
-            movimentoMapped.n_nota = this.formataNumeroNotaFiscal(movimento.entrega.numeros_nf);
-            movimentoMapped.n_ticket = movimento.entrega.numero_ticket;
-            movimentoMapped.placa = movimento.entrega.caminhao.placa;
-          }else{
-            movimentoMapped.n_nota = '-';
-            movimentoMapped.n_ticket = '-';
-            movimentoMapped.placa = '-';
-          }
-
-          if(movimento.tipo.nome === 'Transferencia'){
-            movimentoMapped.is_editable = movimento.transferencia.negocio_cultura_armazem_origem.negocio_cultura.id === this.negocioCultura.id;
-          }
-
-          return movimentoMapped;
-        });
-      }
-    },
     data(){
       return {
         negocioService: new NegocioService(),
         movimentos: null,
         isLoadingMovimentos: false,
-        columns: [
-          {label: 'Tipo', name:'tipo', field:'tipo', align: 'left',},
-          {label: 'Nº Nota', name:'n_nota', field:'n_nota', align: 'left',},
-          {label: 'N° Ticket', name:'n_ticket', field:'n_ticket', align: 'left',},
-          {label: 'Placa', name:'placa', field:'placa', align: 'left',},
-          {label: 'Peso Bruto', name:'p_bruto', field:'p_bruto', align: 'left',},
-          {label: 'Peso Desconto', name:'p_desconto', field:'p_desconto', align: 'left',},
-          {label: 'Peso Líquido', name:'p_liquido', field:'p_liquido', align: 'left',},
-          {label: 'Data', name:'data', field:'data', align: 'left',},
-          {label: '', name:'actions', field:'actions', align: 'right',},
-        ],
       }
     },
     methods:{
@@ -459,7 +420,14 @@
       refreshNegocioMovimentos(){
         this.$root.$emit('refreshNegocio');
         this.listMovimentos();
-      }
+      },
+      entregaUrl(entrega){
+        if(entrega){
+          return '/#/admin/entregas/' + entrega.id;
+        }else{
+          return null;
+        }
+      },
     },
     mounted() {
       this.listMovimentos();
@@ -475,6 +443,13 @@
   .table-container .q-spinner circle{
     stroke-width: 6px;
     color: #8c8c8c;
+  }
+  .table-container .link{
+    text-decoration: none;
+    color: unset;
+  }
+  .table-container .link .q-item:hover{
+    background: hsla(0, 0%, 74%, 0.24);
   }
   .table-header{
     color: rgba(0,0,0,.54);
